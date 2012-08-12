f8c385acaed4921ef252e7016005aff84ca5b76e
t-zhoma <haha8681@126.com>
2012-08-12 18:27:26 +0800

    Prompt function

--- game.js
+++ game.js
@@ -34,9 +34,24 @@
             PAIR: 1,        // 对子
             PAIRS: 2,       // 连对（滚子）
             STRAIGHT: 3,    // 顺子
-            BOMB: 4,        // 炸弹
-            INVALID: 5      // 无效牌
+            THREE_BOMB: 4,  // 小炸弹（三张牌）
+            FOUR_BOMB: 5,   // 大炸弹（四张牌）
+            KING_BOMB: 6,   // 王炸（大小王）
+            INVALID: 7      // 无效牌
         };
+
+        // rule[i][j] means whether PutType i > j
+        // 0: No, i.e. rule[PutType.SINGLE][PutType.KING_BOMB]
+        // 1: Yes, i.e. rule[PutType.KING_BOMB][PutType.SINGLE]
+        // 2: Maybe, i.e. rule[PutType.SINGLE][[PutType.SINGLE]]
+        this.rule = [[2, 0, 0, 0, 0, 0, 0, 0],
+                     [0, 2, 0, 0, 0, 0, 0, 0],
+                     [0, 0, 2, 0, 0, 0, 0, 0],
+                     [0, 0, 0, 2, 0, 0, 0, 0],
+                     [1, 1, 0, 1, 2, 0, 0, 0],
+                     [1, 1, 1, 1, 1, 2, 0, 0],
+                     [1, 1, 1, 1, 1, 1, 0, 0],
+                     [0, 0, 0, 0, 0, 0, 0, 0]];
 
         // level
         this.level = ["3", "4", "5", "6", "7", "8", "9", "10", "j", "q", "k", "a"];
@@ -57,7 +72,7 @@
     Game.prototype.put = function () {
         // TODO
         var selectedCards = new Array;
-        for (i = this.bb.cards.length - 1; i >= 0; i--) {
+        for (var i = this.bb.cards.length - 1; i >= 0; i--) {
             if (this.bb.cards[i].selected) {
                 selectedCards.push(this.bb.cards[i]);
                 this.bb.cards.splice(i, 1);
@@ -82,18 +97,20 @@
 
     Game.prototype.prompt = function () {
         // TODO
-        var lastCard = [new Card(this.suitnames[0], 5, null)]; // "8"
+        var lastCard = [new Card(this.suitnames[1], 1, null), new Card(this.suitnames[0], 1, null),
+                        new Card(this.suitnames[1], 2, null), new Card(this.suitnames[0], 2, null),
+                        new Card(this.suitnames[1], 3, null), new Card(this.suitnames[0], 3, null)];
+        //var lastCard = [new Card("", 13, null)]; // "joker-b"        
         this.choosePrompt(this.bb.cards, lastCard);
         renderer.drawBottomBox();
-        // alert( this.getCardType(lastCard));
     };
 
     Game.prototype.buildImgSrcs = function () {
         var n;
         var si;
         var picname;
-        for (si = 0; si < 4; si++) {
-            for (n = 0; n < 13; n++) {
+        for (var si = 0; si < 4; si++) {
+            for (var n = 0; n < 13; n++) {
                 picname = "img\\" + this.suitnames[si] + "-" + this.cardNums[n] + "-75.png";
                 this.imgSrcs.push(picname);
             }
@@ -109,8 +126,8 @@
         var n;
         var si;
         var src;
-        for (si = 0; si < 4; si++) {
-            for (n = 0; n < 13; n++) {
+        for (var si = 0; si < 4; si++) {
+            for (var n = 0; n < 13; n++) {
                 src = "img\\" + this.suitnames[si] + "-" + this.cardNums[n] + "-75.png";
                 this.deck.push(new Card(this.suitnames[si], n, src));
             }
@@ -137,7 +154,7 @@
     }
 
     Game.prototype.fillbox = function () {
-        for (i = 0; i < 14; i++) {
+        for (var i = 0; i < 14; i++) {
             this.bb.cards.push(new Card(this.deck[i].suit, this.deck[i].num, this.deck[i].src));
         }
     }
@@ -155,9 +172,9 @@
     // Select sort(Desc)
     Game.prototype.sort = function (cards) {
         var max;
-        for (i = 0; i < cards.length - 1; i++) {
+        for (var i = 0; i < cards.length - 1; i++) {
             max = i;
-            for (j = i; j < cards.length; j++) {
+            for (var j = i; j < cards.length; j++) {
                 if (this.getOrder(cards[j]) > this.getOrder(cards[max])) {
                     max = j;
                 }
@@ -179,25 +196,35 @@
         var suitnames = ["diamonds", "clubs", "spades", "hearts"];
         var i = 0;
 
-        for (j = 0; j < curLevel; j++) {
-            for (k = 0; k < suitnames.length; k++) {
-                this.cardOrder[this.cardFullName(suitnames[k], j)] = i++;
+        for (var j = 0; j < curLevel; j++) {
+            for (var k = 0; k < suitnames.length; k++) {
+                this.cardOrder[this.cardFullName(suitnames[k], j)] = i;
             }
+            i++;
         }
 
-        for (j = curLevel + 1; j < this.level.length; j++) {
-            for (k = 0; k < suitnames.length; k++) {
-                this.cardOrder[this.cardFullName(suitnames[k], j)] = i++;
+        for (var j = curLevel + 1; j < this.level.length; j++) {
+            for (var k = 0; k < suitnames.length; k++) {
+                this.cardOrder[this.cardFullName(suitnames[k], j)] = i;
             }
+            i++;
         }
 
-        for (k = 0; k < suitnames.length; k++) {
-            this.cardOrder[this.cardFullName(suitnames[k], 12)] = i++; // "2"
+        for (var k = 0; k < suitnames.length; k++) {
+            if (suitnames[k] == "hearts") {
+                i++;
+            }
+            this.cardOrder[this.cardFullName(suitnames[k], 12)] = i; // "2"
         }
+        i++;
 
-        for (k = 0; k < suitnames.length; k++) {
-            this.cardOrder[this.cardFullName(suitnames[k], curLevel)] = i++;
+        for (var k = 0; k < suitnames.length; k++) {
+            if (suitnames[k] == "hearts") {
+                i++;
+            }
+            this.cardOrder[this.cardFullName(suitnames[k], curLevel)] = i;
         }
+        i++
 
         this.cardOrder[this.cardFullName("", "13")] = i++; // joker-b
         this.cardOrder[this.cardFullName("", "14")] = i++; // joker-r
@@ -211,7 +238,7 @@
                 return this.PutType.SINGLE;
             case 2:
                 if ((cards[0].num == 14 && cards[1].num == 13)) { // "joker-r" and "joker-b"
-                    return this.PutType.BOMB;
+                    return this.PutType.KING_BOMB;
                 }
                 else if (cards[0].num == cards[1].num) {
                     return this.PutType.PAIR;
@@ -219,7 +246,7 @@
                 break;
             case 3:
                 if (cards[0].num == cards[1].num && cards[1].num == cards[2].num) {
-                    return this.PutType.BOMB;
+                    return this.PutType.THREE_BOMB;
                 }
                 else if (this.isStraight(cards)) {
                     return this.PutType.STRAIGHT;
@@ -227,7 +254,7 @@
                 break;
             case 4:
                 if (cards[0].num == cards[1].num && cards[1].num == cards[2].num && cards[2].num == cards[3].num) {
-                    return this.PutType.BOMB;
+                    return this.PutType.FOUR_BOMB;
                 }
                 else if (this.isStraight(cards)) {
                     return this.PutType.STRAIGHT;
@@ -236,6 +263,9 @@
             default:
                 if (this.isPairs(cards)) {
                     return this.PutType.PAIRS;
+                }
+                else if (this.isStraight(cards)) {
+                    return this.PutType.STRAIGHT;
                 }
                 break;
         }
@@ -250,7 +280,7 @@
 
         this.sort(cards);
 
-        for (i = 0; i < cards.length - 1; i++) {
+        for (var i = 0; i < cards.length - 1; i++) {
             if (cards[i].num != cards[i + 1].num + 1) {
                 return false;
             }
@@ -264,7 +294,7 @@
             return false;
 
         this.sort(cards);
-        for (i = 0; i < cards.length - 3; i = i + 2) {
+        for (var i = 0; i < cards.length - 3; i = i + 2) {
             if (cards[i].num != cards[i + 1].num || cards[i].num != cards[i + 2].num + 1 ||
            cards[i].num != cards[i + 3].num + 1) {
                 return false;
@@ -275,7 +305,7 @@
     }
 
     Game.prototype.containSpecialCard = function (cards) {
-        for (i = 0; i < cards.length; i++) {
+        for (var i = 0; i < cards.length; i++) {
             if (cards[i].num == this.getCurrentLevel() || cards[i].num == 12 ||
            cards[i].num == 13 || cards[i].num == 14) { // master, "2", "joker-b" or "joker-r"
                 return true;
@@ -286,7 +316,7 @@
     }
 
     Game.prototype.getLastSelectedIndex = function (cards) {
-        for (i = cards.length - 1; i >= 0; i--) {
+        for (var i = cards.length - 1; i >= 0; i--) {
             if (cards[i].selected) {
                 return i;
             }
@@ -301,39 +331,178 @@
             start = cards.length - 1;
         }
         this.unchooseAll(cards);
-        switch (this.getCardType(lastCards)) {
-            case this.PutType.SINGLE:
-                if (!this.chooseSingle(cards, lastCards, start) && start != cards.length - 1) {
-                    this.chooseSingle(cards, lastCards, cards.length - 1);
+        var j = this.getCardType(lastCards);
+        for (var i = this.PutType.SINGLE; i < this.PutType.INVALID; i++) {
+            if (this.rule[i][j] != 0) {
+                if (this.chooseCard(cards, lastCards, start, i, this.rule[i][j] == 1 ? false : true)) {
+                    return;
                 }
-                break;
-            case this.PutType.PAIR:
-                break;
-            case this.PutType.PAIRS:
-                break;
-            case this.PutType.STRAIGHT:
-                break;
-            case this.PutType.BOMB:
-                break;
+            }
         }
     }
 
     Game.prototype.unchooseAll = function (cards) {
-        for (i = 0; i < cards.length; i++) {
+        for (var i = 0; i < cards.length; i++) {
             cards[i].selected = false;
         }
     }
 
-    Game.prototype.chooseSingle = function (cards, lastCards, start) {
-        for (i = start; i >= 0; i--) {
-            if (this.getOrder(cards[i]) > this.getOrder(lastCards[0])) {
+    Game.prototype.chooseCard = function (cards, lastCards, start, searchType, bCompare) {
+        switch (searchType) {
+            case this.PutType.SINGLE:
+                return this.chooseSingle(cards, lastCards, start, bCompare);
+                break;
+            case this.PutType.PAIR:
+                return this.choosePair(cards, lastCards, start, bCompare);
+                break;
+            case this.PutType.PAIRS:
+                return this.choosePairs(cards, lastCards, start, bCompare);
+                break;
+            case this.PutType.STRAIGHT:
+                return this.chooseStraight(cards, lastCards, start, bCompare);
+                break;
+            case this.PutType.THREE_BOMB:
+                return this.chooseThreeBomb(cards, lastCards, start, bCompare);
+                break;
+            case this.PutType.FOUR_BOMB:
+                return this.chooseFourBomb(cards, lastCards, start, bCompare);
+                break;
+            case this.PutType.KING_BOMB:
+                return this.KingFourBomb(cards, lastCards, start, bCompare);
+                break;
+        }
 
-                cards[i].selected = true;
+        return false;
+    }
+
+    Game.prototype.chooseSingle = function (cards, lastCards, start, bCompare) {
+        for (var i = 0; i < cards.length; i++) {
+            var j = (start - i) % (cards.length);
+            if (!bCompare || this.getOrder(cards[j]) > this.getOrder(lastCards[0])) {
+                cards[j].selected = true;
                 return true;
             }
         }
 
         return false;
+    }
+
+    Game.prototype.choosePair = function (cards, lastCards, start, bCompare) {
+        for (var i = 0; i < cards.length; i++) {
+            var j = (start - i) % (cards.length);
+            if (j < 1) { continue; }
+            if (cards[j].num == cards[j - 1].num) {
+                if (!bCompare || this.getOrder(cards[j]) > this.getOrder(lastCards[0])) {
+                    cards[j].selected = true;
+                    cards[j - 1].selected = true;
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    Game.prototype.choosePairs = function (cards, lastCards, start, bCompare) {
+        for (var i = 0; i < cards.length; i++) {
+            var j = (start - i) % (cards.length);
+            var temp = [];
+            var index = j;
+            var indices = [];
+            do {
+                while (index > 1 && cards[index].num == cards[index - 2].num) { index--; }
+                temp.push(cards[index]);
+                indices.push(index);
+                temp.push(cards[index - 1]);
+                indices.push(index - 1);
+                index -= 2;
+            }
+            while (temp.length < lastCards.length && index > 0)
+
+            if (temp.length < lastCards.length) { continue; }
+
+            if (this.isPairs(temp)) {
+                if (!bCompare || this.getOrder(cards[j]) > this.getOrder(lastCards[lastCards.length - 1])) {
+                    for (var m = 0; m < indices.length; m++) {
+                        cards[indices[m]].selected = true;
+                    }
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    Game.prototype.chooseStraight = function (cards, lastCards, start, bCompare) {
+        for (var i = 0; i < cards.length; i++) {
+            var j = (start - i) % (cards.length);
+            var temp = [];
+            var index = j;
+            var indices = [];
+            do {
+                while (index > 0 && cards[index].num == cards[index - 1].num) { index--; }
+                temp.push(cards[index]);
+                indices.push(index);
+                index--;
+            }
+            while (temp.length < lastCards.length && index >= 0)
+
+            if (temp.length < lastCards.length) { continue; }
+
+            if (this.isStraight(temp)) {
+                if (!bCompare || this.getOrder(cards[j]) > this.getOrder(lastCards[lastCards.length - 1])) {
+                    for (var m = 0; m < indices.length; m++) {
+                        cards[indices[m]].selected = true;
+                    }
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    Game.prototype.chooseThreeBomb = function (cards, lastCards, start, bCompare) {
+        for (var i = 0; i < cards.length; i++) {
+            var j = (start - i) % (cards.length);
+            if (j < 2) { continue; }
+            if (cards[j].num == cards[j - 1].num && cards[j - 1].num == cards[j - 2].num) {
+                if (!bCompare || this.getOrder(cards[j]) > this.getOrder(lastCards[0])) {
+                    cards[j].selected = true;
+                    cards[j - 1].selected = true;
+                    cards[j - 2].selected = true;
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    Game.prototype.chooseFourBomb = function (cards, lastCards, start, bCompare) {
+        for (var i = 0; i < cards.length; i++) {
+            var j = (start - i) % (cards.length);
+            if (j < 3) { continue; }
+            if (cards[j].num != cards[j - 1].num ||
+                cards[j - 1].num != cards[j - 2].num ||
+                cards[j - 2].num != cards[j - 3].num) { continue; }
+
+            if (!bCompare || this.getOrder(cards[j]) > this.getOrder(lastCards[0])) {
+                for (var k = 0; k < 4; k++) { cards[j - k].selected = true; }
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    Game.prototype.KingFourBomb = function (cards, lastCards, start, bCompare) {
+        if (cards.length < 2 || cards[0].num != 14 || cards[1].num != 13) { return false; }
+
+        cards[0].selected = true;
+        cards[1].selected = true;
+        return true;
     }
 
     var Card = function (suit, num, src) {
