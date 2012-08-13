e111088902a9b23bdcb837583e27550523d3a57f
t-zhoma <haha8681@126.com>
2012-08-13 14:31:58 +0800

    Fix issue 2

--- game.js
+++ game.js
@@ -69,17 +69,25 @@
 
     };
 
+    //## Just for emulator
+    var selectedCards = new Array;
+
     Game.prototype.put = function () {
         // TODO
-        var selectedCards = new Array;
+        selectedCards.length = 0;
         for (var i = this.bb.cards.length - 1; i >= 0; i--) {
-            if (this.bb.cards[i].selected) {
-                selectedCards.push(this.bb.cards[i]);
-                this.bb.cards.splice(i, 1);
-            }
+            if (this.bb.cards[i].selected) { selectedCards.push(this.bb.cards[i]); }
         }
 
-        this.sort(selectedCards);
+        if (this.getCardType(selectedCards) == this.PutType.INVALID) {
+            selectedCards.length = 0;
+            smoke.signal("Invalid cards!");
+            return;
+        }
+
+        for (var i = this.bb.cards.length - 1; i >= 0; i--) {
+            if (this.bb.cards[i].selected) { this.bb.cards.splice(i, 1); }
+        }
 
         renderer.drawBottomBox();
         renderer.drawBottomOutbox(selectedCards);
@@ -97,11 +105,7 @@
 
     Game.prototype.prompt = function () {
         // TODO
-        var lastCard = [new Card(this.suitnames[1], 1, null), new Card(this.suitnames[0], 1, null),
-                        new Card(this.suitnames[1], 2, null), new Card(this.suitnames[0], 2, null),
-                        new Card(this.suitnames[1], 3, null), new Card(this.suitnames[0], 3, null)];
-        //var lastCard = [new Card("", 13, null)]; // "joker-b"        
-        this.choosePrompt(this.bb.cards, lastCard);
+        this.choosePrompt(this.bb.cards, selectedCards);
         renderer.drawBottomBox();
     };
 
@@ -534,7 +538,7 @@
         this.h = h;
     }
 
-    var Player = function ( playerId, playerName) {
+    var Player = function (playerId, playerName) {
         this.id = playerId;
         this.name = playerName;
 
